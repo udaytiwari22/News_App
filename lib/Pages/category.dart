2e76b7e821b733af547b7f934d63ce00919bfb1b
Uday Tiwari <udaytiwaricyef2001@gmail.com>
2024-11-26 15:02:48 +0530

    updates

--- lib/Pages/category.dart
+++ lib/Pages/category.dart
@@ -3,7 +3,7 @@ import 'package:fresh_news/Data/news.dart';
 import 'package:fresh_news/models/blogs.dart';
 
 class CategoryNews extends StatefulWidget {
-  final String?category;
+  final String? category;
   CategoryNews({this.category});
   @override
   _CategoryNewsState createState() => _CategoryNewsState();
@@ -11,83 +11,84 @@ class CategoryNews extends StatefulWidget {
 
 class _CategoryNewsState extends State<CategoryNews> {
   List<blogs> articles = [];
-  bool loading=true;
+  bool loading = true;
   @override
-  void initState(){
+  void initState() {
     getCategoryNews();
     super.initState();
   }
-  getCategoryNews() async{
-    CategoryNewsClass news =CategoryNewsClass();
+
+  getCategoryNews() async {
+    CategoryNewsClass news = CategoryNewsClass();
     await news.getnews(widget.category!);
-    articles =news.blog;
-    setState(() {
-      loading =false;
-    });
+    articles = news.blog;
+    setState(
+      () {
+        loading = false;
+      },
+    );
   }
+
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:AppBar(
+      appBar: AppBar(
         automaticallyImplyLeading: false,
-      backgroundColor: Colors.white,
-      shadowColor: Colors.pink,
-      title: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            'Fresh ${widget.category}',
-            style: TextStyle(
-                color: Colors.black
+        backgroundColor: Colors.white,
+        shadowColor: Colors.pink,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Fresh ${widget.category}',
+              style: const TextStyle(color: Colors.black),
             ),
-          ),
-          Text(' News',
-            style: TextStyle(
-                color: Colors.pink
-            ),
-          )
-        ],
+            Text(
+              ' News',
+              style: TextStyle(color: Colors.pink),
+            )
+          ],
+        ),
       ),
-    ),
       body: Container(
         child: Padding(
-          padding: const EdgeInsets.only(top:10.0),
-          child: loading ? Container(
-            child:Center(child: CircularProgressIndicator()),
-          ): SingleChildScrollView(
-            child: Container(
-
-                child:ListView.builder(
-                    physics: NeverScrollableScrollPhysics(),
-                    itemCount: articles.length,
-                    shrinkWrap: true,
-                    itemBuilder: (context,index){
-                      return BlockTile(
-                        imageurl:articles[index].urltoimage ,
-                        title:articles[index].title ,
-                        desc: articles[index].description,
-                        url:articles[index].url,
-                      );
-                    }
+          padding: const EdgeInsets.only(top: 10.0),
+          child: loading
+              ? Container(
+                  child: Center(child: CircularProgressIndicator()),
                 )
-            ),
-          ),
+              : SingleChildScrollView(
+                  child: Container(
+                      child: ListView.builder(
+                          physics: NeverScrollableScrollPhysics(),
+                          itemCount: articles.length,
+                          shrinkWrap: true,
+                          itemBuilder: (context, index) {
+                            return BlockTile(
+                              imageurl: articles[index].urltoimage,
+                              title: articles[index].title,
+                              desc: articles[index].description,
+                              url: articles[index].url,
+                            );
+                          })),
+                ),
         ),
       ),
     );
+  }
 }
-}
+
 class BlockTile extends StatelessWidget {
-  final String? imageurl,title,desc,url;
+  final String? imageurl, title, desc, url;
   BlockTile(
-      {@required this.url,@required this.imageurl,@required this.title,@required this.desc}
-      );
+      {@required this.url,
+      @required this.imageurl,
+      @required this.title,
+      @required this.desc});
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: (){
-        
-      },
+      onTap: () {},
       child: Container(
         child: Column(
           children: [
@@ -96,21 +97,24 @@ class BlockTile extends StatelessWidget {
                 child: Image.network(imageurl!)),
             Padding(
               padding: const EdgeInsets.all(8.0),
-              child: Text(title!, style: TextStyle(
-                fontSize: 20.0,
-                fontWeight:FontWeight.w800,
-                fontStyle:FontStyle.italic,
-
-              ),),
+              child: Text(
+                title!,
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.w800,
+                  fontStyle: FontStyle.italic,
+                ),
+              ),
             ),
             Padding(
               padding: const EdgeInsets.all(8.0),
-              child: Text
-
-                (desc!,style: TextStyle(
-                color: Colors.black54,
-                fontSize: 17.0,
-              ),),
+              child: Text(
+                desc!,
+                style: TextStyle(
+                  color: Colors.black54,
+                  fontSize: 17.0,
+                ),
+              ),
             )
           ],
         ),
